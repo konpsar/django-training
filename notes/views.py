c49ace48c1fff9ca7c62ba27a769b43cb987bd8f
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2022-08-29 14:29:31 +0300

    Fix to display 404 with custom message when trying to query not existing note instead of an error page

--- notes/views.py
+++ notes/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Notes
+from django.http import Http404
+
 
 def list(request):
     all_notes = Notes.objects.all()
     return render(request, 'notes/notes_list.html', {'notes': all_notes})
 
 def detail(request, pk):
-    note = Notes.objects.get(pk=pk)
+    try:        
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("note doesn't exist")
     return render(request, 'notes/notes_detail.html', {'note': note})
