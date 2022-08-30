d9da5328ae00550ce0e72c3312d5224aa1978342
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2022-08-31 02:25:34 +0300

    make smart/notes views class based

--- notes/views.py
+++ notes/views.py
@@ -1,14 +1,22 @@
+from typing import List
 from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import DetailView, ListView
+
+from .models import Notes
 
 # Create your views here.
-from .models import Notes
-from django.http import Http404
 
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
-
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+    
+    
 def detail(request, pk):
     try:        
         note = Notes.objects.get(pk=pk)
