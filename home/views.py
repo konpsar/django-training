9731ed9c5a3583379374aea1fd158776bec4844f
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2022-09-03 00:55:24 +0300

    when an authenticated user tries to access signup page -> redirect to notes list

--- home/views.py
+++ home/views.py
@@ -7,12 +7,18 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 
+from django.shortcuts import redirect
 
 # Create your views here.
 class SignupView(CreateView):
     form_class = UserCreationForm
     template_name = 'home/register.html'
     success_url = '/smart/notes'
+    
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
 
 class LogoutInterfaceView(LogoutView):
     template_name = 'home/logout.html'
