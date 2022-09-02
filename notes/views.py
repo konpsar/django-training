62795f015a3638e1585ed9b4bebc4d301989dc8c
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2022-09-02 23:21:12 +0300

    Make notes list require authentication - after login see only current logged in user's notes

--- notes/views.py
+++ notes/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from .forms import NotesForm
 from .models import Notes
@@ -25,11 +27,18 @@ class NotesCreateView(CreateView):
     success_url = '/smart/notes'
     form_class = NotesForm
     
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
+    login_url = "/admin"
+    
+    #Search on ccbv.co.uk/projects/Django/3.1/ to get help on class views and find what 
+    #we should alter in order to get only specific user's notes
 
+    def get_queryset(self):
+        return self.request.user.notes.all()
+    
 class NotesDetailView(DetailView):
     model = Notes
     context_object_name = "note"
