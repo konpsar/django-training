76d016b2fd3dd5b51cd5424a2815ebda3c3c3b90
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2022-09-03 00:42:00 +0300

    Fix landing page (home) - Ready login/logout views/endpoints. TODO: connect login/out with the rest of sitemap
    TODO: connect login/out with the rest of sitemap

--- home/views.py
+++ home/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/logout.html'
+
 class LoginInterfaceView(LoginView):
     template_name = 'home/login.html'
 
