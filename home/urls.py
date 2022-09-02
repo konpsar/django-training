6886310850c14bb63aad4925a16f8036de3391ca
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2022-09-03 01:00:16 +0300

    Add Login/Logout in navbar

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view()),
     path('login', views.LoginInterfaceView.as_view(), name='login'),
-    path('logout', views.LogoutInterfaceView.as_view(), name='login'),
+    path('logout', views.LogoutInterfaceView.as_view(), name='logout'),
     path('signup', views.SignupView.as_view(), name='signup'),
 ]
